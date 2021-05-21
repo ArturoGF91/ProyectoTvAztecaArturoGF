2a0f997569dd4783aa83ee1ac84873ca40bfc64c
Irving <84554954+ArturoGF91@users.noreply.github.com>
2021-05-21 00:09:02 -0500

    Create InsertarPersonalTvazteca.sql

---
+++ InsertarPersonalTvazteca.sql
@@ -0,0 +1,5 @@
+insert into PersonalTvAzteca values ('Irivng Arturo','Guzman','Flores')
+insert into PersonalTvAzteca values ('Miguel Angel','Guzman','Flores')
+insert into PersonalTvAzteca values ('Juan Eduardo','Guzman','Flores')
+insert into PersonalTvAzteca values ('Aldair','Flores','Huerta')
+insert into PersonalTvAzteca values ('Diana Trinidad','Flores','Sosa')
